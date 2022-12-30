380653e77279911bbd3052bf2148725856681cc6
HyunKyu <yunhk2001@gmail.com>
2022-12-30 20:04:29 +0900

    문자열 대소문자 변환 문장 속 단어 중 가장 긴 단어

---
+++ Algorithm/src/string/a2_대소문자변환.java
@@ -0,0 +1,45 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class a2_대소문자변환 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int len = str.length();
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i = 0; i < len; ++i)
+		{
+			char c = str.charAt(i);
+			
+			if(c > 'Z') {
+				sb.append((char)(c - 32));
+			}
+			else {
+				sb.append((char)(c + 32));
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
+
+/*
+설명
+대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+
+
+입력
+첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+문자열은 영어 알파벳으로만 구성되어 있습니다.
+
+
+출력
+첫 줄에 대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 출력합니다.
+*/
---
+++ Algorithm/src/string/a3_문장속단어.java
@@ -0,0 +1,41 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class a3_문장속단어 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = br.readLine().split(" ");
+		int len = str.length;
+		
+		int maxIdx = 0;
+		int maxLen = 0;
+		
+		for (int i = 0; i < len; i++) {
+			if(str[i].length() > maxLen) {
+				maxIdx = i;
+				maxLen = str[i].length();
+			}
+		}
+		
+		System.out.println(str[maxIdx]);
+	}
+}
+
+/*
+설명
+
+한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요.
+문장속의 각 단어는 공백으로 구분됩니다.
+
+입력
+첫 줄에 길이가 100을 넘지 않는 한 개의 문장이 주어집니다. 문장은 영어 알파벳으로만 구성되어 있습니다.
+
+출력
+첫 줄에 가장 긴 단어를 출력한다. 가장 길이가 긴 단어가 여러개일 경우 문장속에서 가장 앞쪽에 위치한
+단어를 답으로 합니다.
+*/
