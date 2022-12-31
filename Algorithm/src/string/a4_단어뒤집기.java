4e3da03c2bdaca81bbd825ee459743330219b700
HyunKyu <yunhk2001@gmail.com>
2022-12-31 20:47:56 +0900

    단어뒤집기, 특정문자뒤집기

---
+++ Algorithm/src/string/a4_단어뒤집기.java
@@ -0,0 +1,42 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class a4_단어뒤집기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		StringBuffer sb;
+		
+		while(n-- > 0)
+		{
+			String str = br.readLine();
+			int len = str.length();
+			sb = new StringBuffer();
+			for(int i = len-1; i >= 0; --i)
+			{
+				sb.append(str.charAt(i));
+			}
+			
+			System.out.println(sb.toString());
+		}
+	}
+}
+
+/*
+설명
+N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+
+
+입력
+첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+두 번째 줄부터 N개의 단어가 각 줄에 하나씩 주어집니다. 단어는 영어 알파벳으로만 구성되어 있습니다.
+
+
+출력
+N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+*/
---
+++ Algorithm/src/string/a5_특정문자뒤집기.java
@@ -0,0 +1,79 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class a5_특정문자뒤집기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		StringBuilder sb = new StringBuilder();
+		int len = str.length();
+		
+		for (int i = len-1; i >= 0; --i) {
+			if( Character.isAlphabetic(str.charAt(i)) ) {
+				sb.append(str.charAt(i)); 
+			}
+		}
+		
+		int idx = 0;
+		StringBuilder sb2 = new StringBuilder();
+		for(int i = 0; i < len; ++i) {
+			if( Character.isAlphabetic(str.charAt(i)) ) {
+				sb2.append(sb.charAt(idx));
+				idx++;
+			}
+			else {
+				sb2.append(str.charAt(i));
+			}
+		}
+		
+		System.out.println(sb2.toString());
+	}
+	
+	// 다른 풀이
+	public static String solution(String str) {
+		String answer = "";
+		
+		char[] cArray = str.toCharArray();
+		int lt = 0, rt = str.length()-1;
+		while(lt < rt) {
+			if(!Character.isAlphabetic(cArray[lt])) {
+				lt++;
+				continue;
+			}  
+			if(!Character.isAlphabetic(cArray[rt])) {
+				rt--;
+				continue;
+			}
+			
+			char tmp = cArray[lt];
+			cArray[lt] = cArray[rt];
+			cArray[rt] = tmp;
+			lt++;
+			rt--;
+		}
+		
+		answer = String.valueOf(cArray);
+		
+		return answer;
+	}
+}
+
+/*
+설명
+
+영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고,
+특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
+
+
+입력
+첫 줄에 길이가 100을 넘지 않는 문자열이 주어집니다.
+
+
+출력
+첫 줄에 알파벳만 뒤집힌 문자열을 출력합니다.
+*/
