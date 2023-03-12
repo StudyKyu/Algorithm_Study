9e642bb09538f25917e13a505f5d59e5043bc833
HyunKyu <yunhk2001@gmail.com>
2023-03-12 21:59:50 +0900

    bfs

--- Algorithm/src/Recursive_Tree_Graph/a8_송아지찾기_BFS_상태트리탐색.java
+++ Algorithm/src/Recursive_Tree_Graph/a8_송아지찾기_BFS_상태트리탐색.java
@@ -18,9 +18,10 @@ public class a8_송아지찾기_BFS_상태트리탐색 {
 			int len = Q.size();
 			for(int i = 0; i < len; ++i) {
 				int x = Q.poll();
-				if(x == e) return L;
+//				if(x == e) return L;
 				for(int j = 0; j < 3; ++j) {
 					int nx = x + dis[j];
+					if(nx == e) return L+1;
 					if(nx >= 1 && nx <= 10000 && ch[nx] == 0) {
 						ch[nx] = 1;
 						Q.offer(nx);
